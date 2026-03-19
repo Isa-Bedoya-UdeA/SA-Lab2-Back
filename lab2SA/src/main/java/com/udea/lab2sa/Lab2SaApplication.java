504745522b35220c98777b782bd7d4f8f33e25ae
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-03-19 08:48:07 -0500

    Created Java App

---
+++ lab2SA/src/main/java/com/udea/lab2sa/Lab2SaApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2SaApplication.class, args);
+    }
+
+}
